e995ada2e1edc72f24a997ebc0511bbeea0e115c
vavalos4816 <victorandy78@gmail.com>
2018-12-14 16:58:30 -0600

    Add files via upload

---
+++ demo/src/main/java/com/example/beans/Persona.java
@@ -0,0 +1,41 @@
+package com.example.beans;
+
+public class Persona {
+	private int id;
+	private String nombre;
+	private String apodo;
+	private Pais pais;
+	private Ciudad ciudad;
+	
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApodo() {
+		return apodo;
+	}
+	public void setApodo(String apodo) {
+		this.apodo = apodo;
+	}
+	
+}
