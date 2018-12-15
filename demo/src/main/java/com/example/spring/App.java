166f002d81fee24598ae271ec21812ee67eb6d00
Victor de la Cruz <vcg_cruz@hotmail.com>
2018-12-14 20:43:42 -0600

    Fixes

--- demo/src/main/java/com/example/spring/App.java
+++ demo/src/main/java/com/example/spring/App.java
@@ -2,17 +2,16 @@ package com.example.spring;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.example.beans.Persona;
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ApplicationContext ac = new ClassPathXmlApplicationContext("com/example/xml/beans.xml");
-		
-		Persona m = (Persona)ac.getBean("persona");
-		System.out.println(m.getId()+" "+m.getNombre()+" "+m.getApodo()+" "+m.getPais().getNombre()+" "+m.getCiudad().getNombre());
-		((ConfigurableApplicationContext)ac).close();
+		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:com/example/xml/beans.xml");
+
+		Persona m = ac.getBean("persona", Persona.class);
+		System.out.println(m.getId() + " " + m.getNombre() + " " + m.getApodo() + " " + m.getPais().getNombre() + " " + m.getPais().getCiudad().getNombre());
+		((ConfigurableApplicationContext) ac).close();
 	}
 }
